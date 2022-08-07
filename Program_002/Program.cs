d53224904dc582ba42d3b8cb3ace0007ac0e4888
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-07 22:33:32 +0400

    Программа по выдаче дня недели согласно запрошенному числу

--- Program_002/Program.cs
+++ Program_002/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа которая выдает день недели согласно запрашиваемому числу
+
+Console.WriteLine("Напишите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x==1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (x==2)
+{
+    Console.WriteLine("Вторник");
+}
+if (x==3)
+{
+    Console.WriteLine("Среда");
+}
+if (x==4)
+{
+    Console.WriteLine("Четверг");
+}
+if (x==5)
+{
+    Console.WriteLine("Пятница");
+}
+if (x==6)
+{
+    Console.WriteLine("Суббота");
+}
+if (x==7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+Console.WriteLine("Ввели неверное значение");
