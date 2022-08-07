d95515ff4e06d18ef2dcaa1236ff58a64f077469
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-07 23:30:07 +0400

    Программа определения максимального из двух чисел

--- Program_006/Program.cs
+++ Program_006/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Введите два числа и определить какое больше
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num2 > num1)
+{
+    Console.WriteLine("Максимальное число: ");
+    Console.Write(num2);
+}
+else
+{
+    Console.WriteLine("Максимальное число: ");
+    Console.Write(num1);
+}
