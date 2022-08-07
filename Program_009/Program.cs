ed138f3e1815b6a075d4371102824dd7e1748f03
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-08 00:30:49 +0400

    решение не верное

--- Program_009/Program.cs
+++ Program_009/Program.cs
@@ -1,2 +1,14 @@
 ﻿// Ввести число N, и показать все четные числа от 1 до N
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count < n)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+count++;
+}
