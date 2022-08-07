0c8c5fa38c59bdb6d677516c8f4622241134a75c
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-07 23:12:34 +0400

    Программа показывающая последнюю цифру от числа

--- Program_005/Program.cs
+++ Program_005/Program.cs
@@ -1,7 +1,7 @@
 ﻿// 
 Console.WriteLine("Введите число:");
 int x = Convert.ToInt32(Console.ReadLine());
-int z == x % 10;
+int z = x % 10;
 Console.WriteLine("Последняя цифра числа: ");
 Console.Write(z);
 
