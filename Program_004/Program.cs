6cf2820358a9c405bbeacb4d6cf4071c6de18ca3
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-07 23:05:43 +0400

    Программа показывающая все числа N от - до +

--- Program_004/Program.cs
+++ Program_004/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа на вход принимает одно число а на выходе показывает в промежутке от -n до n
+
+Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0 - a;
+
+while (b <= a)
+{
+    Console.Write(b);
+    Console.Write(" ");
+    b++;
+}
