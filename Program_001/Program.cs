2fe0e6ce9d149bc33318e1b29c73b5deb8664576
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-07 22:20:08 +0400

    Программа расчитыванияквадрата введеного числа

---
+++ Program_001/Program.cs
@@ -0,0 +1,7 @@
+﻿//Программа на вход принимает число и выдает его квадрат 
+ 
+Console.WriteLine("Укажите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = n * n;
+Console.WriteLine("Квадрат числа: ");
+Console.WriteLine(result);
