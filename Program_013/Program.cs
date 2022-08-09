33c99e98540b2728c7f1f31dcf93405cbd7325c7
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-09 22:35:59 +0400

    Перепроверить

---
+++ Program_013/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num = new Random().Next(10, 100);
+Console.WriteLine(num);
+int a = 7;
+int b = 23;
+
+if (num % a == 0 && num % b == 0)
+{
+    Console.WriteLine("ДА");
+}
+else
+{
+    Console.WriteLine("НЕТ");
+}
