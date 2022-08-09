b0ee1a1cf8c14e989a00abd6f629ad2daf3a5de9
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-09 22:43:51 +0400

    Что-то написал

---
+++ Program_014/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Напишите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напишите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2 * num2)
+{
+    Console.WriteLine("Являеься квадратом числа");
+}
+else
+{
+    Console.WriteLine("НЕ является квадратом числа");
+}
