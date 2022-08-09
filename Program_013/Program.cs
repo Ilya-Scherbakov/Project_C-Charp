a270f2f640c59aacbeb02fa9ce75624d2e0b6716
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-10 00:02:27 +0400

    одновременная проверка кратности числа на 23 и 7

--- Program_013/Program.cs
+++ Program_013/Program.cs
@@ -1,15 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// На ввод принимает число и проверяет кратность одновременно 7 и 23
 
 int num = new Random().Next(10, 100);
 Console.WriteLine(num);
+
 int a = 7;
 int b = 23;
 
 if (num % a == 0 && num % b == 0)
 {
-    Console.WriteLine("ДА");
+    Console.WriteLine($"Число: {num} кратно одновременно 23 и 7");
 }
 else
 {
-    Console.WriteLine("НЕТ");
+    Console.WriteLine($"Число: {num} НЕ кратно одновременно 23 и 7");
 }
