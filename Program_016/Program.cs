5d4ef5c057eae9603ff534eef7b952814b6741f2
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-10 01:26:42 +0400

    У рандомного числа выводит третью цифру, если ее нет то пишет нет

--- Program_016/Program.cs
+++ Program_016/Program.cs
@@ -1,5 +1,17 @@
 ﻿// Выводит третью цифру заданного числа или сообщает что ее нет
 
-int num = new Random().Next(10, 100000);
+int num = new Random().Next(10, 10000);
 Console.WriteLine(num);
 
+if (num > 100)
+{
+    while (num > 1000)
+    {
+        num = num / 10;
+    }
+    Console.WriteLine($"Третья цифра: {num % 10}");
+}
+else
+{
+    Console.WriteLine("Третей цифры нет");
+}
