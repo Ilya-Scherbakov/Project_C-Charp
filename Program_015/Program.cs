b5e8c77c5170b6a2443df92cf1f87dc2322bf3d2
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-10 00:24:17 +0400

    На ввод трехзначное число и вывести вторую цифру

---
+++ Program_015/Program.cs
@@ -0,0 +1,7 @@
+﻿// На ввод принимает трехзначное число и показывает вторую цифру. Напр: 235 -> 3
+
+int num = new Random().Next(100, 1000);
+Console.WriteLine(num);
+int x = num / 10 - ((num / 100) * 10);
+
+Console.WriteLine($"Второе число будет: {x}");
