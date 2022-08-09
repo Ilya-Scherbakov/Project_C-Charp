ee81c6862b5de973b8e62d4e2e8a08f308facf7b
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-09 23:42:50 +0400

    Рандомное 3х значное число, из него убрать десятки и оставить сотни и единицы

--- Program_010/Program.cs
+++ Program_010/Program.cs
@@ -4,10 +4,10 @@ Console.WriteLine("Hello, World!");
 int num = new Random().Next(10,100);
 Console.WriteLine(num);
 int a = num / 10; // десятки
-int b = num % 10; // единицы
-int max = a;
-if (max < b)
-{
-    max = b;
-}
-Console.WriteLine(max);
+//int b = num % 10; // единицы
+//int max = a;
+//if (max < b)
+//{
+//    max = b;
+//}
+Console.WriteLine(a);
--- Program_011/Program.cs
+++ Program_011/Program.cs
@@ -1,5 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Укажите число:");
-int n = Convert.ToInt32(Console.ReadLine());
-int a = num / 100;
-int b = ()
+﻿// Программа вводит случайное стрехзначное число и удаляет вторую цифру напр: 238 -> 28
+
+int num = new Random().Next(100, 1000);
+
+// Первый способ
+// int a = num / 100;
+// int b = (num - a * 100) / 10;
+// int c = num % 10; // остаток при делении на десять. Допустим 123 / 10 = 12,3, значит запишет 3
+
+// Console.WriteLine($"Число: {num}. Первая цифра: {a}, Вторая цифра: {b}, Третья цифра: {c}.");
+// Console.WriteLine($"{a}{c}");
+
+//Второй способ (я не понял)
+Console.WriteLine(num);
+int result = num / 100 * 10 + num % 10;
+Console.WriteLine(result);
