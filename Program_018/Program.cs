42414131676b8ca4cf8fbc198a61ca74b20a69ce
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-10 23:00:48 +0400

    У рандомного числа выводит третью цифру, если ее нет то пишет нет только решение через  функцию

---
+++ Program_018/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int ProverkaChisla(int chislo)
+{
+    int colChisel = 0;
+    while (chislo > 0)
+    {
+        chislo = chislo / 10;
+        colChisel++;
+    }
+    return colChisel;
+}
+
+int number = 123456;
+int sumchislo = ProverkaChisla(number);
+int control = sumchislo - 3;
+int i = 0;
+
+if (sumchislo >= 3)
+{
+    while (i < control)
+    {
+        number = number / 10;
+        i++;
+    }
+    int thirdNumber = number % 10;
+    Console.WriteLine($"Третья цифра: {thirdNumber}");
+
+}
+if (sumchislo < 3)
+{
+    Console.WriteLine($"Нету цифирки");
+
+}
