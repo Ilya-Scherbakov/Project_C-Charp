83b29253d75291d16d7d4aee85e7ba74a2b9722a
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-12 23:30:21 +0400

    задача с семинара про упорядоченнось цифр в числе

---
+++ Program_019/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int max = n % 10;
+
+while (n > 10)
+{
+    if (max > (n / 10) % 10)
+    {
+        n = n / 10;
+        max = n % 10;
+    }
+    else
+    {
+        Console.WriteLine("Число не упорядоченое");
+    }
+}
+Console.Write("Число упорядоченное");
