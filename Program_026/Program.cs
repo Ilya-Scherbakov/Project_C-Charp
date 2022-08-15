c4095c1f788ff446efa6a521585c2acb1aa69d5a
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-15 20:13:53 +0400

    Таблица кубов от 1 до N

---
+++ Program_026/Program.cs
@@ -0,0 +1,11 @@
+﻿// На ввод число N, а на выходе таблица кубов этих чисел
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= n)
+{
+    Console.WriteLine(Math.Pow(count,3));
+    count++;
+}
