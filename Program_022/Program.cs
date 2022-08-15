8c05ed3dad7d76a0757687ac1e31b9eeb7be3a89
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-15 18:41:48 +0400

    Программа рассчета длины между точками

---
+++ Program_022/Program.cs
@@ -0,0 +1,13 @@
+﻿// Принимает на вход координаты двух точек и находит расстояние между ними в 2D
+Console.Write("Введите координату X точки А: ");
+int xA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y точки А: ");
+int yA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату X точки B: ");
+int xB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y точки B: ");
+int yB = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow((xA - xB),2) + Math.Pow((yA - yB),2));  // Math.Pow(),2 - формула степени, через "," указывается в какую степень
+                                                                      // Math.Sqrt() - формула квадратного корня
+Console.WriteLine($"промежуточный результат: {d.ToString("F" + 2)}"); // ToString("F" + 2) - комнда округления до 2го знака по математическим законам        
