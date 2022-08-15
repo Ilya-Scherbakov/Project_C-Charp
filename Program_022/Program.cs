c4fee6372c887dbddb70bffa1064d7ffef6e09e3
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-15 20:12:43 +0400

    Расстояние точек в 2D

--- Program_022/Program.cs
+++ Program_022/Program.cs
@@ -8,6 +8,6 @@ int xB = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координату Y точки B: ");
 int yB = Convert.ToInt32(Console.ReadLine());
 
-double d = Math.Sqrt(Math.Pow((xA - xB),2) + Math.Pow((yA - yB),2));  // Math.Pow(),2 - формула степени, через "," указывается в какую степень
+double d = Math.Sqrt(Math.Pow((xA - xB),2) + Math.Pow((yA - yB),2));  // Math.Pow( ,2) - формула степени, через "," указывается в какую степень
                                                                       // Math.Sqrt() - формула квадратного корня
 Console.WriteLine($"Расстояние: {d.ToString("F" + 2)}"); // ToString("F" + 2) - комнда округления до 2го знака по математическим законам        
