5ec8bfced49abbbf258e52cc0778dbddb2489cc3
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-15 17:17:33 +0400

    По заданным координатам выдает четверть

---
+++ Program_020/Program.cs
@@ -0,0 +1,28 @@
+﻿// На ввод принимает координаты точки (X и Y), причем х=!0 и у=!0, и выдает номер четверти плоскости в которой находится
+Console.Write("Введите координату X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if ((x == 0) || y == 0)
+{
+    Console.WriteLine("Не может быть равен нулю");
+}
+
+else if ((x > 0) && (y > 0))
+{
+    Console.WriteLine("Точка в первой четверти");
+}
+else if ((x < 0) && (y > 0))
+{
+    Console.WriteLine("Точка во второй четверти");
+}
+else if ((x < 0) && (y < 0))
+{
+    Console.WriteLine("В третей четверти");
+}
+else if ((x > 0) && (y < 0))
+{
+    Console.WriteLine("В четвертой четверти");
+}
+
