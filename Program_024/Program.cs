bd0143da96332f4cd31f4332452ca88afd97f918
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-15 19:40:21 +0400

    На ввод пятизначное число с проверкой на палиндром

--- Program_024/Program.cs
+++ Program_024/Program.cs
@@ -1,2 +1,23 @@
 ﻿// На ввод пятизначное число и проверяет является ли оно палиндромом
 Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = (n / 10000) % 10;
+int b = (n / 1000) % 10;
+int c = (n / 10) % 10;
+int d = n % 10;
+
+if (n > 9999 && n < 100000)
+{
+    if (a == d && b == c)
+    {
+        Console.WriteLine("Является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("НЕ является");
+    }
+}
+else
+{
+    Console.WriteLine("Ввели неверное значение");
+}
