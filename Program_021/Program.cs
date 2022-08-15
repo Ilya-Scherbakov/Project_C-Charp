6b8aa8f991b8ca7c6f7bc6886c6f6b82611cfe46
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-15 17:55:52 +0400

    Программа по номеру четверти говорит какой диапазон координат может быть

---
+++ Program_021/Program.cs
@@ -0,0 +1,27 @@
+﻿// Программма по заданному номеру четверти показывает возможные координаты
+
+Console.WriteLine("Введите номер четверти от 1 до 4х");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n <= 0 || n > 4)
+{
+    Console.WriteLine("Ввели неверное значение");
+}
+
+else if (n == 1)
+{
+    Console.WriteLine("Диапазон х > 0 и у > 0");
+}
+else if (n == 2)
+{
+    Console.WriteLine("Диапазон х < 0 и у > 0");
+}
+else if (n == 3)
+{
+    Console.WriteLine("Диапазон х < 0 и у < 0");
+}
+else if (n == 4)
+{
+    Console.WriteLine("Диапазон х > 0 и у < 0");
+}
+
