fbed1c98c7ff0bbabc5d31bb2aaa0428602000f7
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-15 18:58:10 +0400

    чейто не пашет

---
+++ Program_023/Program.cs
@@ -0,0 +1,12 @@
+﻿// На вход число N и выдает таблицу квадратов чисел
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+sum = 1;
+
+while (sum <= n)
+{
+    Console.WriteLine(sum * sum);
+    sum++;
+}
+Console.WriteLine();
