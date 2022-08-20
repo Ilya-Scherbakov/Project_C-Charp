d874681374de44f5e5e9fba9a93e913a31d1dee2
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-20 23:06:17 +0400

    Вывод массива заполненный 0 и 1 ПОКА НЕ РАБОТАЕТ

---
+++ Program_029/Program.cs
@@ -0,0 +1,17 @@
+﻿// Выводит массив из 8 элемнтов, заполненный 0 и 1 в случайном порядке.
+
+void FillArray(int[] arr)
+{
+    int i = 0;
+    for (int length = arr.length; i < length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+        Console.WriteLine($" {arr[i]} ");
+    }
+}
+
+Console.Write("Введите длину массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+
+FillArray(array);
