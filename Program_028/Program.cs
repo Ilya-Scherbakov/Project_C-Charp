6cc36f3f296f6285ba6cc789477befd6eed398ac
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-20 19:43:58 +0400

    на ввод число а она считает сколько цифр в числе

---
+++ Program_028/Program.cs
@@ -0,0 +1,15 @@
+﻿// На ввод принимает число и выдает количество цифр в числе
+
+void SumNum(int num)
+{
+    int i = 1;
+    for (i = 1; num > 10; i++)
+    {
+        num = num / 10;
+    }
+    Console.WriteLine($"количество цифр в числе: {i}");
+}
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+SumNum(a);
