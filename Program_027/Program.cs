13102b17a48741d503a895c01159781eb9b79bc6
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-20 19:01:03 +0400

    Две задачи: сумма и произведение от 1 до N

--- Program_027/Program.cs
+++ Program_027/Program.cs
@@ -1,21 +1,36 @@
 ﻿// На вход принимает число "А" и выдает сумму числел от 1 до А. ЧЕРЕЗ МАССИВЫ И ФУНКЦИИ
 
-void ArfProg(int num)
+// void ArfProg(int num)
+// {
+//     int sum = 0;
+//     for (int i = 1; i <= num; i++)
+//     {
+//         sum = sum + i;
+//     }
+//     Console.WriteLine($"сумма чисел равна: {sum}");
+// }
+
+// Console.Write("Введите число A: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// ArfProg(a);
+// Console.Write("Введите число B: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// ArfProg(b);
+// Console.Write("Введите число C: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+// ArfProg(c);
+
+// На вход принимает число N и выдает произведение от 1 до N
+
+void GeoProg(int num)
 {
-    int sum = 0;
+    int sum = 1;
     for (int i = 1; i <= num; i++)
     {
-        sum = sum + i;
+        sum = sum * i;
     }
-    Console.WriteLine($"сумма чисел равна: {sum}");
+    Console.WriteLine($"геометрическая прогрессия будет равна: {sum}");
 }
-
-Console.Write("Введите число A: ");
-int a = Convert.ToInt32(Console.ReadLine());
-ArfProg(a);
-Console.Write("Введите число B: ");
-int b = Convert.ToInt32(Console.ReadLine());
-ArfProg(b);
-Console.Write("Введите число C: ");
-int c = Convert.ToInt32(Console.ReadLine());
-ArfProg(c);
+Console.Write("Введите число N:");
+int n = Convert.ToInt16(Console.ReadLine());
+GeoProg(n);
