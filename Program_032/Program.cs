20ea224a4af4696466c0f4fef94d2fa37ac6973a
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-21 01:17:50 +0400

    ДЗ 29 псделать массив из 8 цифр

---
+++ Program_032/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задает массив из 8 эл-ов и выдает их на экран
+
+void FillArray(int[] arr)
+{
+    Console.Write("[");
+    int i = 0;
+    for (int length = arr.Length; i < length; i++)
+    {
+        arr[i] = new Random().Next(0, 100);
+        Console.Write($" {arr[i]}, ");
+    }
+    Console.Write("]");
+}
+
+// Console.Write("Введите длину массива: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int[] array = new int[N];
+
+int[] array = new int[8];
+
+FillArray(array);
