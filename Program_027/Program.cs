c44946ce37da9294c9ba61f0c8306320522f8c23
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-20 11:32:50 +0400

    Сумма чисел от 1 до А через функцию

---
+++ Program_027/Program.cs
@@ -0,0 +1,21 @@
+﻿// На вход принимает число "А" и выдает сумму числел от 1 до А. ЧЕРЕЗ МАССИВЫ И ФУНКЦИИ
+
+void ArfProg(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    Console.WriteLine($"сумма чисел равна: {sum}");
+}
+
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+ArfProg(a);
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+ArfProg(b);
+Console.Write("Введите число C: ");
+int c = Convert.ToInt32(Console.ReadLine());
+ArfProg(c);
