f94da94363a720bdd94a32fd679e7a13cd68eefa
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-23 00:43:53 +0400

    Задачас семинара не доделал

---
+++ Program_035/Program.cs
@@ -0,0 +1,34 @@
+﻿// 32. Замена эл-ов массива положительных на отрицательные
+
+int[] rococo = new int[10];
+
+for (int i = 0; i < rococo.Length; i++)
+{
+    rococo[i] = new Random().Next(-9, 10);
+    Console.Write($" {rococo[i]}");
+    if (rococo[i] < 0)
+    {
+        rococo[i] = rococo[i] * -1;
+    }
+    else
+    {
+        rococo[i] = rococo[i] * -1;
+    }
+}
+
+Console.WriteLine($"новый массив: {rococo[i]}");
+
+// Пробую сам так
+// int Func(int arr[])
+// for (int i = 0; i < rococo.Length; i++)
+// {
+//     rococo[i] = new Random().Next(-9, 10);
+//     Console.Write($" {rococo[i]}");
+//     return;
+// }
+
+// Func(rococo[10])
+// rococo[i] = - rococo[i];
+// Console.Write($" {rococo[i]}");
+
+// int[] rococo = new int[10];
