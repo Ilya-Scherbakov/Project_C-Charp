c17f122b89340971c75295097ae76210c45708ac
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-23 00:43:11 +0400

    Задача с семинара не доделал

---
+++ Program_036/Program.cs
@@ -0,0 +1,29 @@
+﻿// 33. Определяет присутствует ли определенное число в массиве
+
+int[] array = new int[10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-9, 10);
+    Console.Write($" {array[i]}");
+    
+}
+Console.WriteLine("Напишите циферку:");
+int num = Convert.ToInt32(Console.ReadLine);
+bool start = false;
+for (int i = 0; i < array.Length; i++)
+{
+    if (num == array[i])
+        {
+            start = true;
+        }
+    else
+    {
+        start = false;
+    }
+}
+Console.WriteLine();
+if (start == true)
+Console.Write("Данное число есть");
+else
+Console.Write("Нет");
