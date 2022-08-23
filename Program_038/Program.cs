82d682c5f1732faa1504dd1db3a038481c66edbb
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-23 19:24:40 +0400

    Задача 37. Не доделал

---
+++ Program_038/Program.cs
@@ -0,0 +1,28 @@
+﻿// Найти произведение пар чисел в массиве. Парой считаем первый и последний 
+// Результат записать в новом массиве
+int[] array = new int[9];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 20);
+    Console.Write($" {array[i]} ");
+}
+Console.WriteLine();
+// for (int i = 0; i < array.Length / 2; i++)
+//     Console.Write($" {array[i] * array[array.Length - 1 - i]}");
+
+void resultMult(int[] massif)
+{
+    for (int i = 0; i < massif.Length / 2; i++)
+    if (massif.Length % 2 == 0)
+    {   
+        Console.Write($" {massif[i] * massif[massif.Length - 1 - i]} ");
+    }
+    else
+    {
+        Console.Write($" {massif[i] * massif[massif.Length - 1 - i]} ");
+        Console.Write(massif[massif.Length / 2 ]);
+    }
+}
+
+resultMult(array);
