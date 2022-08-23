bfcdf1d58cff95c605b1a64f7e679b9fb4c1ab18
Ilya-Scherbakov <scherbak89@yandex.ru>
2022-08-23 18:54:35 +0400

    массив из 123 чисел, нати колличестов эл-ов в определенном диапазоне

---
+++ Program_037/Program.cs
@@ -0,0 +1,14 @@
+﻿// 35. Задать одномерный массив из 123 чисел, найти колличество эл-ов массива значения которых лежат 
+// в отрезке от [10, 99]
+
+int[] array = new int[12];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 200);
+    Console.Write($" {array[i]} ");
+    if (array[i]>=10 && array[i]<=99)
+    count++;
+}
+Console.WriteLine();
+Console.WriteLine($" кол-во эл-ов: {count}");
